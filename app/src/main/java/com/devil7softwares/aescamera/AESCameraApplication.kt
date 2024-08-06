d624f2e6681abe9692266682c0b1e2fb3adbd4a7
Devil7DK <dineshthangavel47@gmail.com>
2024-08-06 14:41:30 +0530

    Clear key if no activity is open for more than 5 seconds

--- app/src/main/java/com/devil7softwares/aescamera/AESCameraApplication.kt
+++ app/src/main/java/com/devil7softwares/aescamera/AESCameraApplication.kt
@@ -1,6 +1,10 @@
 package com.devil7softwares.aescamera
 
+import android.app.Activity
 import android.app.Application
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.lifecycle.MutableLiveData
 import java.io.File
 
@@ -34,7 +38,7 @@ class AESCameraApplication : Application() {
             }
 
             if (value != null && mediaDir != null && mediaDir.exists()) {
-                val keyDir = File(mediaDir, value) .apply { mkdirs() }
+                val keyDir = File(mediaDir, value).apply { mkdirs() }
 
                 if (keyDir.exists()) {
                     this.outputDirectory = keyDir
@@ -46,7 +50,7 @@ class AESCameraApplication : Application() {
             }
         }
     var outputDirectory: File? = null
-        private set (value) {
+        private set(value) {
             field = value
 
             if (value != null) {
@@ -55,4 +59,44 @@ class AESCameraApplication : Application() {
         }
     var thumbnailDirectory: File? = null
         private set
+
+    private var activeActivities = 0
+    private val handler = Handler(Looper.getMainLooper())
+    private val clearKeyDelay = 5000L
+
+    fun onActivityStarted() {
+        activeActivities++
+        handler.removeCallbacksAndMessages(null)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacks {
+            override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {}
+            override fun onActivityStarted(activity: Activity) {
+                activeActivities++
+                handler.removeCallbacks(clearKeyRunnable)
+            }
+
+            override fun onActivityResumed(activity: Activity) {}
+            override fun onActivityPaused(activity: Activity) {}
+            override fun onActivityStopped(activity: Activity) {
+                onActivityStopped()
+            }
+
+            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
+            override fun onActivityDestroyed(activity: Activity) {}
+        })
+    }
+
+    private fun onActivityStopped() {
+        activeActivities--
+        if (activeActivities == 0) {
+            handler.postDelayed(clearKeyRunnable, clearKeyDelay)
+        }
+    }
+
+    private val clearKeyRunnable = Runnable {
+        key = null
+    }
 }
